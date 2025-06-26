c504b2e30aba4193ecf2f439b2bc4afb24542d6d
gene <eugenefedchenco@gmail.com>
2025-06-26 22:43:25 +0300

    Add sender base

--- src/sender.ts
+++ src/sender.ts
@@ -1,4 +1,26 @@
+// Containers
+
+const states: Record<string, any> = {};
+
+// Listeners
+
+window.addEventListener('message', (event: MessageEvent) => {
+	// Todo: validate source
+	emitState(event.data.name);
+});
+
+// Functions
+
 function setFrameState(name: string, value: any) {
-	// Todo: secure origin
-	window.parent.postMessage({ name, value }, '*');
+	states[name] = value;
+	emitState(name);
 }
+
+function emitState(name: string) {
+	// Todo: secure origin
+	window.parent.postMessage({ name, value: states[name] }, '*');
+}
+
+// Exports
+
+export default setFrameState;
