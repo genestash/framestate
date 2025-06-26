a4d404262f7fd762d2d68d4d8609c6c60b8f5f6d
gene <eugenefedchenco@gmail.com>
2025-06-26 22:18:59 +0300

    Add receiver base

---
+++ src/receiver.ts
@@ -0,0 +1,20 @@
+// Imports
+
+// Containers
+
+const states: Record<string, any> = {};
+const listeners: Record<string, Set<VoidFunction>> = {};
+
+// Listeners
+
+window.addEventListener('message', (event: MessageEvent) => {});
+
+// Functions
+
+function subscribe(name: string, callback: VoidFunction) {}
+
+// Hooks
+
+function useFrameState(name: string, initialValue: any) {}
+
+// Exports
