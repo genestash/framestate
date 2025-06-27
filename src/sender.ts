da5e14562df27a0b456e304bee4eb3a6b9482660
gene <eugenefedchenco@gmail.com>
2025-06-27 21:27:46 +0300

    Add typescript

--- src/sender.ts
+++ src/sender.ts
@@ -4,30 +4,24 @@ const states: Record<string, any> = {};
 
 // Listeners
 
-window.addEventListener('message', (event: MessageEvent) => {
-    if (!event.data.isFrameState) return;
-    const name: string = event.data.name;
-    if (!states.hasOwnProperty(name)) return;
+window.addEventListener('message', (event: MessageEvent<{ name: string }>) => {
+    // Todo: validate source
+    const name = event.data.name;
+    if (!states.hasOwn(name)) return;
     emitState(name);
 });
 
 // Functions
 
 function setFrameState(name: string, value: any) {
-    if (!Object.is(states[name], value)) return;
+    if (states[name] === value) return;
     states[name] = value;
     emitState(name);
 }
 
 function emitState(name: string) {
-    const data = {
-        name: name,
-        value: states[name],
-        isFrameState: true
-    };
-
     // Todo: secure origin
-    window.parent.postMessage(data, '*');
+    window.parent.postMessage({ name, value: states[name] }, '*');
 }
 
 // Exports
