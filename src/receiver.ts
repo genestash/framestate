8b0f86dcbeb109fc896f36e1b9d758086e57357d
gene <eugenefedchenco@gmail.com>
2025-06-27 23:46:17 +0300

    Add state update logic

--- src/receiver.ts
+++ src/receiver.ts
@@ -7,7 +7,14 @@ const listeners: Record<string, Set<VoidFunction>> = {};
 
 // Listeners
 
-window.addEventListener('message', (event: MessageEvent) => {});
+window.addEventListener('message', (event: MessageEvent<{ name: string; value: any }>) => {
+    // Todo: validate source
+    const name = event.data.name;
+    const value = event.data.value;
+    if (states[name] === value) return;
+    states[name] = value;
+    // Todo: notify subscribers
+});
 
 // Functions
 
