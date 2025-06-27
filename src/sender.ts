e16196770f46dd8214cda757c962aeb1e1a6bc2b
gene <eugenefedchenco@gmail.com>
2025-06-27 20:49:15 +0300

    Update prettier config

--- src/sender.ts
+++ src/sender.ts
@@ -5,29 +5,29 @@ const states: Record<string, any> = {};
 // Listeners
 
 window.addEventListener('message', (event: MessageEvent) => {
-	if (!event.data.isFrameState) return;
-	const name: string = event.data.name;
-	if (!states.hasOwnProperty(name)) return;
-	emitState(name);
+    if (!event.data.isFrameState) return;
+    const name: string = event.data.name;
+    if (!states.hasOwnProperty(name)) return;
+    emitState(name);
 });
 
 // Functions
 
 function setFrameState(name: string, value: any) {
-	if (!Object.is(states[name], value)) return;
-	states[name] = value;
-	emitState(name);
+    if (!Object.is(states[name], value)) return;
+    states[name] = value;
+    emitState(name);
 }
 
 function emitState(name: string) {
-	const data = {
-		name: name,
-		value: states[name],
-		isFrameState: true,
-	};
+    const data = {
+        name: name,
+        value: states[name],
+        isFrameState: true,
+    };
 
-	// Todo: secure origin
-	window.parent.postMessage(data, '*');
+    // Todo: secure origin
+    window.parent.postMessage(data, '*');
 }
 
 // Exports
