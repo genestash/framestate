9f431926a13a81158b2f565fbd25611ae9f29982
gene <eugenefedchenco@gmail.com>
2025-06-28 03:06:36 +0300

    Complete sender logic

--- src/sender.ts
+++ src/sender.ts
@@ -1,27 +1,43 @@
+// Types
+
+interface ReceiverData {
+    getFrameState: {
+        name: string;
+    };
+}
+
 // Containers
 
 const states: Record<string, any> = {};
 
 // Listeners
 
-window.addEventListener('message', (event: MessageEvent<{ name: string }>) => {
-    // Todo: validate source
-    const name = event.data.name;
-    if (!states.hasOwn(name)) return;
+window.addEventListener('message', (event: MessageEvent<ReceiverData>) => {
+    if (!event.data.getFrameState) return;
+    const name = event.data.getFrameState.name;
     emitFrameState(name);
 });
 
 // Functions
 
+function emitFrameState(name: string) {
+    if (!states.hasOwnProperty(name)) return;
+
+    const data = {
+        frameState: {
+            name: name,
+            value: states[name]
+        }
+    };
+
+    // TODO: secure origin
+    window.parent.postMessage(data, '*');
+}
+
 function setFrameState(name: string, value: any) {
     if (states[name] === value) return;
     states[name] = value;
     emitFrameState(name);
-}
-
-function emitFrameState(name: string) {
-    // Todo: secure origin
-    window.parent.postMessage({ name, value: states[name] }, '*');
 }
 
 // Exports
